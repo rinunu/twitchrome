4dfe02a01f17e5a13f106de90685ab3a8c97414a
rinunu <rintaro.t@gmail.com>
2010-06-13 00:55:07 +0900

    忘れ物

---
+++ static/users.js
@@ -0,0 +1,20 @@
+/**
+ * user のリストを表す List 
+ */
+tw.Users = function(url){
+    tw.ServerList.call(this, url);
+};
+
+util.extend(tw.Users, tw.ServerList);
+
+// override
+tw.Users.prototype.toStatuses = function(json){
+    var result = [];
+    for(var i = 0; i < json.length; i++){
+	var user = json[i];
+	var status = user.status;
+	status.user = user;
+	result.push(status);
+    }
+    return result;
+};
