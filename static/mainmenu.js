df2305cf60e35ad8982e63a1af8471405939d062
rinunu <rintaro.t@gmail.com>
2010-06-24 04:23:22 +0900

    些細

--- static/mainmenu.js
+++ static/mainmenu.js
@@ -4,10 +4,12 @@
 tw.MainMenu = function(){
     this.element_ = $(".main.menu");
     var this_ = this;
-    this.element_.find("a.search").click(
-	function(){
-	    var text = this_.element_.find("input.search").val();
-	    tw.showTimeline(tw.store.search(text));
-	});
+    this.element_.find("a.search").click(util.bind(this, this.onSearch));
+    this.element_.find(".search form").submit(util.bind(this, this.onSearch));
+};
 
+tw.MainMenu.prototype.onSearch = function(event){
+    event.preventDefault();
+    var text = this.element_.find("input.search").val();
+    tw.showTimeline(tw.store.search(text));
 };
