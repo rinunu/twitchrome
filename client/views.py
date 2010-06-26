1f6e3c6110fc5d0a0e454ab4f0edf20450881d52
rinunu <rintaro.t@gmail.com>
2010-06-26 21:49:15 +0900

    Django-norel を使用するように変更しました

--- client/views.py
+++ client/views.py
@@ -11,8 +11,6 @@ from django.http import HttpResponse
 from django.http import HttpResponseForbidden
 from django.core.urlresolvers import reverse
 
-from appengine_utilities.sessions import Session
-
 import oauth2 as oauth
 import urllib
 
@@ -29,34 +27,28 @@ logger = logging.getLogger("views")
 
 def login(request):
     # "oauth_callback": request.build_absolute_uri("authenticated")
-    session = Session()
-    
     resp, content = client.request(request_token_url, "GET")
     if resp['status'] != '200':
         raise Exception("Invalid response from Twitter.")
 
-    session['request_token'] = dict(cgi.parse_qsl(content))
+    request.session['request_token'] = dict(cgi.parse_qsl(content))
 
     url = "%s?%s" % (
         authenticate_url,
         urllib.urlencode({
-                "oauth_token": session['request_token']['oauth_token']
+                "oauth_token": request.session['request_token']['oauth_token']
                 }))
     return HttpResponseRedirect(url)
 
 def logout(request):
-    session = Session()
-
-    session.delete()
+    request.session.clear()
     return HttpResponseRedirect('/')
 
 # リクエストトークンを認可された
 def authenticated(request):
-    session = Session()
-
     # リクエストトークン
-    token = oauth.Token(session['request_token']['oauth_token'],
-                        session['request_token']['oauth_token_secret'])
+    token = oauth.Token(request.session['request_token']['oauth_token'],
+                        request.session['request_token']['oauth_token_secret'])
     client = oauth.Client(consumer, token)
 
     # アクセストークンをリクエスト
@@ -74,7 +66,7 @@ def authenticated(request):
         'screen_name': 'aaaaaaaaaaa'
     }
     """
-    session['access_token'] = dict(cgi.parse_qsl(content))
+    request.session['access_token'] = dict(cgi.parse_qsl(content))
 
     return HttpResponseRedirect(reverse(main))
 
@@ -92,10 +84,10 @@ def create_twitter(session):
 # twitter_api を実行し、結果を返す
 # url は http://api.twitter.com/1/ 以降
 def twitter_api(request, url):
+    # return HttpResponse("error", status=404)
+
     if not settings.DEBUG and not request.is_ajax():
         return HttpResponseForbidden("error")
-    
-    session = Session()
     
     # return HttpResponse(url + " - " + str(len(request.GET)))
 
@@ -107,7 +99,7 @@ def twitter_api(request, url):
     for key, value in src_params.iteritems():
         params[key] = value.encode('utf-8')
 
-    t = create_twitter(session)
+    t = create_twitter(request.session)
 
     url = 'http://api.twitter.com/1/' + url;
 
@@ -123,10 +115,8 @@ def index(request):
     return render_to_response("index.html", {})
 
 def main(request):
-    session = Session()
-
-    if 'access_token' not in session:
+    if 'access_token' not in request.session:
         return HttpResponseRedirect(reverse(index))
     
     return render_to_response("main.html", {
-            'screen_name': session['access_token']['screen_name']})
+            'screen_name': request.session['access_token']['screen_name']})
