8694cd9b82c44d3b91645b1ce8977316735c0fb2
rinunu <rintaro.t@gmail.com>
2010-07-06 10:45:38 +0900

    途中

---
+++ static/js/loader.js
@@ -0,0 +1,10 @@
+
+/**
+ * 画像の先読みを行う
+ */
+util.Loader = function(){
+};
+
+
+util.Loader.load = function(uri, callback){
+};
---
+++ static/js/taskmanager.js
@@ -0,0 +1,6 @@
+
+/**
+ * タスク管理を行う
+ */
+tw.TaskManager = function(){
+};
