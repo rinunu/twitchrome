3a59aa587ffa2954e932d76ccddcb1073e959c4e
rinunu <rintaro.t@gmail.com>
2010-07-25 15:42:37 +0900

    IE8 での見た目を整えました

--- static/js/console.js
+++ static/js/console.js
@@ -1,9 +1,15 @@
-if(!window.console){
-    var names = ["log", "debug", "info", "warn", "error", "assert", "dir", "dirxml",
-		 "group", "groupEnd", "time", "timeEnd", "count", "trace", "profile", "profileEnd"];
-    
-    window.console = {};
-    for(var i = 0; i < names.length; ++i){
-        console[names[i]] = function(){};
-    }
-}
+(function(){
+     if(!window.console){
+	 window.console = {};
+     }
+
+     var names = ["log", "debug", "info", "warn", "error", "assert", "dir", "dirxml",
+		  "group", "groupEnd", "time", "timeEnd", "count", "trace", "profile", "profileEnd"];
+     
+     for(var i = 0; i < names.length; ++i){
+	 var name = names[i];
+	 if(!console[name]){
+             console[name] = function(){};
+	 }
+     }
+})();
